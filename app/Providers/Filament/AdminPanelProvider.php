ceeb72d5804f0d8665b8d80b156d616dcd54c3c6
mazensabry2712 <mazensoftware30@gmail.com>
2025-02-28 02:50:09 +0200

    Add role-based middleware aliases for enhanced authorization control

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Enums\RolesEnum;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -50,6 +51,10 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+                sprintf( 'role:%s|%s',
+            RolesEnum::Admin->value,
+                    RolesEnum::Vendor->value,
+            ),
             ])
             ->authMiddleware([
                 Authenticate::class,
